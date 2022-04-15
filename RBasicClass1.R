5557312aa93f5d5ff037908a3bf446d37c4d5ecf
Austin <austiny@snu.ac.kr>
2022-04-15 19:30:57 +0900

    rr

---
+++ RBasicClass1.R
@@ -0,0 +1,20 @@
+dim(available.packages())
+
+sessionInfo()
+
+install.packages("stringr")
+
+library("stringr")
+
+search()  # 현재 로드된 패키지 확인인
+
+remove.packages("stringr")
+
+data()
+
+
+hist(Nile)
+
+# 변수
+
+
